b7b3b97d46536d51ffa2fdfaa7836b7b444946d8
rendra05 <raendraaa@gmail.com>
2025-06-09 16:08:55 +0800

    Melengkapi method matematika

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -3,15 +3,30 @@ package hitung;
 public class Matematika {
     // membuat variabel
     private double bil1, bil2;
-    
+
     // membuat constructor
     public Matematika(double bil1, double bil2){
         this.bil1 = bil1;
         this.bil2 = bil2;
     }
-    
-    // membuat method setPenjumlahan
+
+    // membuat method penjumlahan
     double setPenjumlahan(){
         return bil1 + bil2;
     }
+
+    // membuat method pengurangan
+    double setPengurangan(){
+        return bil1 - bil2;
+    }
+
+    // membuat method perkalian
+    double setPerkalian(){
+        return bil1 * bil2;
+    }
+
+    // membuat method pembagian
+    double setPembagian(){
+        return bil1 / bil2;
+        }
 }
